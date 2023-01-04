69f9145255b69a2d46314bf176cfbc0d54b72fb9
Rokas Andreikenas <rokas.andreikenas@exacaster.com>
2023-01-04 20:30:02 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./components/Logo";
+import NavigationBar from "./components/NavigationBar";
+import FuncButton from "./components/FuncButton";
+import FuncDiv from "./components/FuncDiv";
+import ClassButton from "./components/ClassButton";
+import ClassDiv from "./components/ClassDiv";
+import Button from "./components/Button";
+
+// App.js yra pagrindinis komponentas
+// Vienas failas = vienas komponentas
+// Komponentai rašomi didžiąją raide
+// Komponentai būna dviejų tipų: funkciniai ir klasiniai
+//   Funkciniai atrodo kaip funkcijos tik iš didžiosios raidės. Regular arba arrow functions
+// Return dalyje rašomas JSX - javascript + html
+// class => className
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Logo />
+      <NavigationBar />
+      <FuncButton title="Spausk mane" color="red" />
+      <FuncButton title="Nespausk prašau" />
+      <FuncDiv title="Kažkas" subtitle="Kažkur kažkas" buttonText="Kas" />
+      <ClassButton title="Klasinis mygtukas" />
+      <ClassDiv title="Kažkas" subtitle="Kažkur kažkas" buttonText="Kas" />
+      <Button>Rokas Spausk čia</Button>
     </div>
   );
 }
